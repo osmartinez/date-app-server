94416835f72dc4b2fe4ba784c9d1b867f7fda67f
Oscar Martinez <oscarmtzz22@gmail.com>
2020-09-21 23:34:17 +0200

    corrige deprecation warnings en mongoose

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ const env = process.env.NODE_ENV || 'production'
 const config = require('./config')
 const morgan = require('morgan')
 const port = config[env].server.port
+const mongoose = require('mongoose')
 
 const app = express()
+mongoose.connect('mongodb://' + config[env].database.host + '/' + config[env].database.name, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
-
-if (env === 'development'){
+if (env === 'development') {
     app.use(morgan('dev'))
 }
 
 const router = require('./src/routes/index')()
 app.use('/api', router)
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`[${env}] REST service up and running on port ${port}`)
 })
--- src/repository/generic.repository.js
+++ src/repository/generic.repository.js
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose');
 const config = require('../../config')
 const env = process.env.NODE_ENV || 'production'
 
 
 
-mongoose.connect('mongodb://' + config[env].database.host + '/' +config[env].database.name);
 
 function GenericRepository(){
     function execute(dbContext, parameters, req,res,next){
